8369bc6b5a1a0be5964c1dbc9da1f5a26f45b82f
Matthew Sawyer <57121937+matthewleonardsawyer@users.noreply.github.com>
2020-02-15 13:28:03 -0500

    Removed a comment and fixed a link issue in tables.html

--- tables.js
+++ tables.js
@@ -1,1 +1,11 @@
-{}
+// this.id = id;
+// this.name = name;
+// this.email = email;
+// this.phone = phone;
+
+const fakeArray = {
+    id : 34,
+    name : "Fred",
+    email : "Fred@fake.email",
+    phone : "4044044040"
+}
